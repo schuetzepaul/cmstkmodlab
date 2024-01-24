5bae8762814950df2bd88b20702c5f4bf8103c8b
Paul Schuetze <paul.schuetze@desy.de>
2024-01-24 13:55:36 +0100

    Start implementing input dialogue for scanning component IDs - non-functional so far

--- assembly/assemblyCommon/AssemblyAssemblyV2.h
+++ assembly/assemblyCommon/AssemblyAssemblyV2.h
@@ -167,6 +167,8 @@ class AssemblyAssemblyV2 : public QObject
 
   // others
 
+  void ScanMaPSAID_start();
+
   void RegisterPSSPlusSpacersToMaPSAPosition_start();
   void RegisterPSSPlusSpacersToMaPSAPosition_finish();
 
@@ -179,6 +181,8 @@ class AssemblyAssemblyV2 : public QObject
   // motion
   void move_absolute_request(const double, const double, const double, const double);
   void move_relative_request(const double, const double, const double, const double);
+
+  void ScanMaPSAID_finished();
 
   void GoToSensorMarkerPreAlignment_finished();
 
